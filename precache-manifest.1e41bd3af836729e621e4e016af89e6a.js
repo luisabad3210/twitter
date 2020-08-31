0f0d1f71e3f328df9bca618a89b6c5bf11fd4040
Luis <luisabad3210@gmail.com>
2020-08-31 16:04:02 -0400

    Updates

---
+++ precache-manifest.1e41bd3af836729e621e4e016af89e6a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c7f20180d3f469af7a459d5662a2994",
+    "url": "/twitter/index.html"
+  },
+  {
+    "revision": "b8551839a3f93ebf6964",
+    "url": "/twitter/static/css/main.5eb51641.chunk.css"
+  },
+  {
+    "revision": "4b41ae88d5909ffbbe36",
+    "url": "/twitter/static/js/2.f9365b30.chunk.js"
+  },
+  {
+    "revision": "170392c3acad6bbffec743442b1d0ac3",
+    "url": "/twitter/static/js/2.f9365b30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8551839a3f93ebf6964",
+    "url": "/twitter/static/js/main.eccaa7e2.chunk.js"
+  },
+  {
+    "revision": "afa0f5d5c42030d9bf92",
+    "url": "/twitter/static/js/runtime-main.6625fca2.js"
+  },
+  {
+    "revision": "2ce4d6a24c74000785ed49d80db92664",
+    "url": "/twitter/static/media/background.2ce4d6a2.jpg"
+  },
+  {
+    "revision": "298eb8eb77cc4cb657ffe4ed2440247d",
+    "url": "/twitter/static/media/bitcoin.298eb8eb.png"
+  },
+  {
+    "revision": "b3f93697315a5f4a8ff6c443b389d8e1",
+    "url": "/twitter/static/media/profile.b3f93697.jpg"
+  }
+]);
