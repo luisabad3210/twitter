c7f9a58403a1855395094c60b08f859d4f64aab8
Luis <luisabad3210@gmail.com>
2020-09-02 18:23:54 -0400

    fixing API issue and adding json files

--- src/components/APIs/TrendingApi.js
+++ src/components/APIs/TrendingApi.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Data from './TrendingData.json';
 
 class TrendingApi extends React.Component {
 
@@ -9,27 +10,30 @@ class TrendingApi extends React.Component {
         }
     }
 
-    componentDidMount() {
-        fetch('https://newsapi.org/v2/top-headlines?country=us&apiKey=6107f6de55684cbab80484c839fbee1e')
-        .then((response) => {
-            return response.json();
-        })
-        .then((myJson) => {
-            this.setState({articles: myJson.articles});
-        });
-    }
+
+    // I was using googles news api but had to change into a json file instead //
+
+
+    // componentDidMount() {
+    //     fetch('https://newsapi.org/v2/top-headlines?country=us&apiKey=6107f6de55684cbab80484c839fbee1e')
+    //     .then((response) => {
+    //         return response.json();
+    //     })
+    //     .then((myJson) => {
+    //         this.setState({articles: myJson.articles});
+    //         console.log(this.state.articles)
+    //     });
+    // }
 
     render() {
-        // console.log(this.state)
         return (
             <div className='trending-container'>
                 <div className='trends-for-you'><h2>Whats Happening</h2> <div>Powered by Google News</div></div>
                 <hr></hr>
-                {this.state.articles.map((item, index) => {
+                {Data.map((item, index) => {
                     return (
                         <div key={index} className='trending'>
                         <h5 className="trendingTitle">{item.title}</h5>
-                        {/* <img alt='' src={item.urlToImage} width='120' height='120'/> */}
                         <div className="trendingBody">{item.description}</div>
                         <form action={item.url}>
                             <button className='Btn' type="submit">Read More</button>
--- src/components/APIs/TweetsApi.js
+++ src/components/APIs/TweetsApi.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import BitcoinImg from '../Images/bitcoin.png';
+import Data from './TweetData.json';
+
 
 class TweetsApi extends React.Component {
 
@@ -10,25 +12,30 @@ class TweetsApi extends React.Component {
         }
     }
 
-    componentDidMount() {
-        fetch('https://newsapi.org/v2/everything?q=bitcoin&sortBy=publishedAt&apiKey=6107f6de55684cbab80484c839fbee1e')
-        .then((response) => {
-            return response.json();
-        })
-        .then((myJson) => {
-            this.setState({tweets: myJson.articles});
-        });
-    }
+
+    // I was using googles news api but had to change into a json file instead //
+
+    
+
+    // componentDidMount() {
+    //     fetch('https://newsapi.org/v2/everything?q=bitcoin&sortBy=publishedAt&apiKey=6107f6de55684cbab80484c839fbee1e')
+    //     .then((response) => {
+    //         return response.json();
+    //     })
+    //     .then((myJson) => {
+    //         this.setState({tweets: myJson.articles});
+    //         console.log(this.state.tweets)
+    //     });
+    // }
 
     render() {
         return (
             <div>
-                {this.state.tweets.map((item, index) => {
+                {Data.map((item, index) => {
                     return (
                         <div key={index} className='tweets'>
-                            <img className='bitcoinImg' alt="bitcoin profile pic" src={BitcoinImg} width='50' height='50'/><i className="postName"> Bitcoin {item.publishedAt.slice(0,10)}</i>
-                            <div className='title'>{item.title}</div>
-                            {/* <div>{item.url}</div> */}
+                            <img className='bitcoinImg' alt="bitcoin profile pic" src={BitcoinImg} width='50' height='50'/><i className="postName"> Bitcoin {item.date.slice(0,10)}</i>
+                            <div className="title">{item.title}</div>
                             <img className='TweetImg' alt='' src={item.urlToImage} />
                             <div className='tweetBody' >{item.description}</div>
                             <button className='Btn'>Comment</button>
